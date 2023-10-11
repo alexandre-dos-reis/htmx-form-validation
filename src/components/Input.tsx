0c9aa4fc3c16110f7946ee865e6934a5f5931486
Alexandre Dos Reis <alexandre@finishers.com>
2023-10-11 11:12:17 +0200

    ok

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,25 +1,38 @@
 interface Props extends JSX.HtmlInputTag {
   label?: string;
   errors?: Array<string>;
-  validationUrl?: string;
+  validate?: {
+    verb: "put" | "post" | "get" | "delete";
+    url: string;
+    triggerOn?: "keyup" | "blur";
+  };
 }
 
-export const Input = ({ label, errors, validationUrl, ...p }: Props) => {
+export const Input = ({ label, errors, validate, ...p }: Props) => {
   const wrapperId = `${p.name}-input-wrapper`;
   const errorId = `${p.name}-error`;
   const inputId = `${p.name}-input`;
 
+  const divHxTags = validate
+    ? {
+        [`hx-${validate.verb}`]: validate.url,
+        "hx-select": `#${wrapperId}`,
+        "hx-target": "this",
+        "hx-trigger": "keyup changed delay:1s from:find input",
+        "hx-swap": "outerHTML",
+        "hx-include": `#${inputId}`,
+      }
+    : {};
+
+  const inputHxTags = validate
+    ? {
+        "hx-preserve": true,
+        autofocus: "true",
+      }
+    : {};
+
   return (
-    <div
-      id={wrapperId}
-      hx-get={validationUrl}
-      hx-select={`#${wrapperId}`}
-      hx-target="this"
-      hx-trigger="blur from:find input"
-      hx-swap="outerHTML"
-      hx-include={`#${inputId}`}
-      class="mb-8"
-    >
+    <div id={wrapperId} class="mb-8" {...divHxTags}>
       {label ? (
         <label for={inputId} class="block text-gray-700 text-sm font-bold mb-2">
           {label}
@@ -29,6 +42,7 @@ export const Input = ({ label, errors, validationUrl, ...p }: Props) => {
         id={inputId}
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         {...p}
+        {...inputHxTags}
       />
       {errors ? (
         <div class="text-red-500" id={errorId}>
--- src/index.tsx
+++ src/index.tsx
@@ -7,50 +7,84 @@ import { BaseHtml } from "./components/BaseHtml";
 
 const app = new Elysia()
   .use(html())
-  .get("/", ({ query, path, set }) => {
+  .all("/", async ({ body, path, set, request }) => {
     const schema = z.object({
       name: z.string().min(3),
-      email: z.string().email(),
+      age: z.preprocess(
+        (val) => parseInt(val as string, 10),
+        z.number().min(18, "You must be above 18 !")
+      ),
+      email: z
+        .string()
+        .email()
+        .refine(async (email) => (email === "john@doe.com" ? false : true), {
+          message: "Email is already taken",
+        }),
     });
+    console.log({ DATA: body });
 
     let errors: FormError<typeof schema> = null;
 
-    if (isObjectSet(query)) {
-      const parsedSchema = schema.safeParse(query);
+    if (request.method === "POST" && isObjectSet(body)) {
+      const parsedSchema = await schema.safeParseAsync(body);
 
       if (!parsedSchema.success) {
         errors = parsedSchema.error.flatten().fieldErrors;
       } else {
-        set.redirect = "/form-completed";
+        if (!request.headers.has("hx-request")) {
+          set.redirect = "/form-completed";
+          return;
+        }
       }
     }
 
     // http://hernantz.github.io/inline-form-validation-with-django-and-htmx.html
     return (
-      <BaseHtml class="bg-black">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <BaseHtml>
+        <h1 class="text-center">HTMX form</h1>
+        <form
+          novalidate
+          method="post"
+          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
           <Input
             label="Email"
             name="email"
             type="email"
-            value={query?.email ?? ""}
+            value={body?.email ?? ""}
             errors={errors?.email}
-            validationUrl={path}
+            validate={{
+              url: path,
+              verb: "post",
+            }}
           />
           <Input
             label="Name"
             name="name"
             type="text"
-            value={query?.name ?? ""}
+            value={body?.name ?? ""}
             errors={errors?.name}
-            validationUrl={path}
+            validate={{
+              url: path,
+              verb: "post",
+            }}
           />
-          <button
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          <Input
+            label="Age"
+            name="age"
+            type="text"
+            value={body?.age ?? ""}
+            errors={errors?.age}
+            validate={{
+              url: path,
+              verb: "post",
+            }}
+          />
+          <input
             type="submit"
-          >
-            Submit
-          </button>
+            value="Submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          />
         </form>
       </BaseHtml>
     );
@@ -58,12 +92,12 @@ const app = new Elysia()
   .get("/form-completed", () => {
     return (
       <BaseHtml>
-        <div>FORM COMPLETED !</div>
+        <h1>FORM COMPLETED !</h1>
       </BaseHtml>
     );
   })
   .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
